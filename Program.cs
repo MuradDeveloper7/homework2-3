f0209eee40867f377bc5bcfc0f0c89daf56fbd4e
murod383 <130088278+murod383@users.noreply.github.com>
2023-10-26 04:29:53 +0100

    INFO;2nd homewirk is ready

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+System.Console.WriteLine("Enter your Currency");
+int yourCurrency = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter todays value to your currency");
+int realCurrency = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"Cuurency for today is; {yourCurrency * realCurrency}  ");
